832613a2d71aaf3b64dd5a827933c09fd78511c6
Nasaee <nasaee.dev@gmail.com>
2025-08-19 22:29:31 +0700

    implement cursor effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import HeroSection from "@/components/sections/HeroSection";
-import Navbar from "@/components/Navbar";
-import LangFontProvider from "@/components/wrappers/LangFontProvider";
+import HeroSection from '@/components/sections/HeroSection';
+import Navbar from '@/components/Navbar';
+import LangFontProvider from '@/components/wrappers/LangFontProvider';
 
 function HomePage() {
   return (
-    <div className="flex flex-col">
+    <div className='flex flex-col'>
       <Navbar />
       <LangFontProvider>
         <HeroSection />
--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,22 +1,22 @@
-"use client";
+'use client';
 
-import { useLangStore } from "@/store/useLangStore";
-import { motion } from "motion/react";
-import { TypeAnimation } from "react-type-animation";
-import Spline from "@splinetool/react-spline";
+import { useLangStore } from '@/store/useLangStore';
+import { motion } from 'motion/react';
+import { TypeAnimation } from 'react-type-animation';
+import Spline from '@splinetool/react-spline';
 
 const textLang = {
   en: {
     headerGreetText: "Hi I'm ",
-    name: "Nasaee",
+    name: 'Nasaee',
     subHeader:
       "I'm a full-stack developer with a strong passion for building modern, responsive, and user-friendly web applications. My expertise includes React, Node.js, and other cutting-edge web technologies. I enjoy creating clean, efficient solutions and I’m always learning to stay ahead of industry trends. Let’s collaborate and bring your ideas to life!",
   },
   th: {
     headerGreetText: "Hi I'm ",
-    name: "Nasaee",
+    name: 'Nasaee',
     subHeader:
-      "ผมเป็นนักพัฒนา Full-stack ที่หลงใหลในการสร้างเว็บแอปที่ทันสมัย ตอบสนองไว และใช้งานง่าย ความเชี่ยวชาญของผมรวมถึง React, Node.js และเทคโนโลยีเว็บใหม่ ๆ ผมชอบสร้างโซลูชันที่เรียบง่ายและมีประสิทธิภาพ พร้อมเรียนรู้อย่างต่อเนื่องเพื่อนำเทรนด์อุตสาหกรรม มาร่วมกันทำให้ไอเดียของคุณเป็นจริงครับ!",
+      'ฉันเป็นนักพัฒนาแบบฟูลสแตก (Full-Stack Developer) ที่มีความมุ่งมั่นอย่างแรงกล้าในการสร้างเว็บแอปพลิเคชันที่ทันสมัย ตอบสนองฉับไว และใช้งานง่าย ความเชี่ยวชาญของฉันครอบคลุม React, Node.js และเทคโนโลยีเว็บล้ำสมัยอื่นๆ ฉันชอบสร้างสรรค์โซลูชันที่สะอาดตาและมีประสิทธิภาพ และกำลังเรียนรู้อยู่เสมอเพื่อก้าวล้ำนำหน้าเทรนด์ในอุตสาหกรรม มาร่วมมือกันและนำไอเดียของคุณมาสร้างสรรค์ให้เป็นจริง!',
   },
 };
 
@@ -25,65 +25,65 @@ function HeroSection() {
   const textData = textLang[lang];
 
   return (
-    <section className="h-screen bg-gradient-to-b from-violet-900 to-black  lg:px-24 px-10 relative overflow-hidden">
-      <div className="container flex xl:flex-row flex-col-reverse items-center justify-between mx-auto h-full">
+    <section className='h-screen bg-gradient-to-b from-violet-900 to-black  lg:px-24 px-10 relative overflow-hidden'>
+      <div className='container flex xl:flex-row flex-col-reverse items-center justify-between mx-auto h-full'>
         {/* Left Section */}
-        <div className="z-40 xl:mb-0 mb-[20%]">
+        <div className='z-40 xl:mb-0 mb-[20%]'>
           <motion.h1
             initial={{ opacity: 0, y: 80 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{
-              type: "spring",
+              type: 'spring',
               stiffness: 40,
               damping: 25,
               delay: 1.3,
               duration: 1.5,
             }}
-            className="text-4xl md:text-7xl lg:text-8xl font-bold z-10 mb-6 lg:tracking-wider"
+            className='text-4xl md:text-7xl lg:text-8xl font-bold z-10 mb-6 lg:tracking-wider'
           >
             {textData.headerGreetText}
             {/* typing text */}
             <TypeAnimation
               key={lang}
               sequence={[
-                "",
+                '',
                 1000,
                 textData.name,
                 2000,
-                "",
+                '',
                 3000,
-                "Fullstack Developer",
+                'Fullstack Developer',
                 4000,
-                "",
+                '',
               ]}
-              wrapper="span"
-              cursor={true}
+              wrapper='span'
+              cursor={false} // close default cursor, custom manually on global css
               repeat={Infinity}
               speed={10}
-              className="hidden lg:inline-block"
+              className='hidden lg:inline-block type'
             />
-            <span className="lg:hidden">{textData.name}</span>
+            <span className='lg:hidden'>{textData.name}</span>
           </motion.h1>
 
           <motion.p
             initial={{ opacity: 0, y: 80 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{
-              type: "spring",
+              type: 'spring',
               stiffness: 40,
               damping: 25,
               delay: 1.8,
               duration: 1.5,
             }}
-            className="text-md md:text-1xl lg:text-2xl text-purple-200 max-w-2xl"
+            className='text-md md:text-1xl lg:text-2xl text-purple-200 max-w-2xl'
           >
             {textData.subHeader}
           </motion.p>
         </div>
         {/* Right Section */}
         <Spline
-          className="absolute xl:right-[-28%] right-0 top-[-20%] lg:top-0"
-          scene="https://prod.spline.design/xjQFy-JzKcwAc59B/scene.splinecode"
+          className='absolute xl:right-[-28%] right-0 top-[-20%] lg:top-0'
+          scene='https://prod.spline.design/xjQFy-JzKcwAc59B/scene.splinecode'
         />
       </div>
     </section>
