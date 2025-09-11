e9c695e70dbb03ce247e4c4a89b7238b20f0d96a
Nasaee <nasaee.dev@gmail.com>
2025-09-11 13:37:08 +0700

    update skill

--- assets/skills/index.ts
+++ assets/skills/index.ts
@@ -1,39 +1,40 @@
-import JsImg from './javascript.svg';
-import skillsImg from './skills.svg';
-import htmlImg from './html.svg';
-import cssImg from './css.svg';
-import tailwindImg from './tailwind-css.svg';
-import reactImg from './react.svg';
-import sassImg from './sass.svg';
-import nodeImg from './nodejs.svg';
-import figmaImg from './figma-icon.svg';
-import bootstrapImg from './bootstrap.svg';
-import postgresqlImg from './postgresql.svg';
-import typescriptImg from './typescript.png';
-import expressImg from './express.png';
-import sqlImg from './database.png';
-import nestImg from './nestjs.svg';
-import dockerImg from './docker.svg';
-import graphQlImg from './graphql.svg';
-import linuxImg from './linux.svg';
-import mongoDBImg from './mongodb.svg';
-import prismaImg from './prisma.png';
-import nextImg from './nextjs.png';
-import reduxImg from './redux.svg';
-import reduxSagaImg from './redux-saga.svg';
-import zodImg from './zod.svg';
-import reactHookFormImg from './react-hook-form.svg';
-import reactQueryIcon from './react-query.svg';
-import muiImg from './MUI.png';
-import shadcnUI from './shadcn.png';
-import zustandImg from './zustand.svg';
-import dartImg from './dart.png';
-import flutter from './flutter.png';
-import nginxImg from './nginx.png';
-import vpsImg from './vps.png';
-import goImg from './go.png';
-import ginImg from './gin.webp';
-import redisImg from './redis.svg';
+import JsImg from "./javascript.svg";
+import skillsImg from "./skills.svg";
+import htmlImg from "./html.svg";
+import cssImg from "./css.svg";
+import tailwindImg from "./tailwind-css.svg";
+import reactImg from "./react.svg";
+import sassImg from "./sass.svg";
+import nodeImg from "./nodejs.svg";
+import figmaImg from "./figma-icon.svg";
+import bootstrapImg from "./bootstrap.svg";
+import postgresqlImg from "./postgresql.svg";
+import typescriptImg from "./typescript.png";
+import expressImg from "./express.png";
+import sqlImg from "./database.png";
+import nestImg from "./nestjs.svg";
+import dockerImg from "./docker.svg";
+import graphQlImg from "./graphql.svg";
+import linuxImg from "./linux.svg";
+import mongoDBImg from "./mongodb.svg";
+import prismaImg from "./prisma.png";
+import nextImg from "./nextjs.png";
+import reduxImg from "./redux.svg";
+import reduxSagaImg from "./redux-saga.svg";
+import zodImg from "./zod.svg";
+import reactHookFormImg from "./react-hook-form.svg";
+import reactQueryIcon from "./react-query.svg";
+import muiImg from "./MUI.png";
+import shadcnUI from "./shadcn.png";
+import zustandImg from "./zustand.svg";
+import dartImg from "./dart.png";
+import flutter from "./flutter.png";
+import nginxImg from "./nginx.png";
+import vpsImg from "./vps.png";
+import goImg from "./go.png";
+import ginImg from "./gin.webp";
+import redisImg from "./redis.svg";
+import playWriteImg from "./Playwrite.svg";
 
 export {
   skillsImg,
@@ -72,4 +73,5 @@ export {
   vpsImg,
   ginImg,
   redisImg,
+  playWriteImg,
 };
